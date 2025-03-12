d0d4b2975307084661bba4115b9eee35b9d35f35
Jan Brehmer <brehmeja+git@gmail.com>
2025-03-12 16:36:51 +0100

    :wrench: added email and github links to unsuccessful result info text

---
+++ secret.js
@@ -0,0 +1,6 @@
+function makeEmailLink(id) {
+  var a = document.getElementById(id);
+  if (!a) return;
+  a.innerHTML = window.atob("YnJlaG1lamEraW9AZ21haWwuY29t");
+  a.href = window.atob("bWFpbHRvOmJyZWhtZWphK2lvQGdtYWlsLmNvbQ==");
+}

575d222d2af958ca6de4721bf955d2694e159fdf
Jan Brehmer <brehmeja+git@gmail.com>
2025-03-12 16:50:18 +0100

    :wrench: added default subject to email link

--- secret.js
+++ secret.js
@@ -1,6 +1,7 @@
 function makeEmailLink(id) {
   var a = document.getElementById(id);
-  if (!a) return;
+  if (!a) return false;
   a.innerHTML = window.atob("YnJlaG1lamEraW9AZ21haWwuY29t");
   a.href = window.atob("bWFpbHRvOmJyZWhtZWphK2lvQGdtYWlsLmNvbQ==");
+  return true;
 }
